33a62e867ad0340b5239c46d1c925928af149c93
manuelzu17 <manuel3783822@gmail.com>
2023-03-02 13:01:53 -0500

    final correccion

--- divition_numbers.py
+++ divition_numbers.py
@@ -1,15 +1,14 @@
+import functools
+
 def find_divisible_numbers(numbers, divisors):
     result = []
     for num in numbers:
-        divisible = True
-        for div in divisors:
-            if num % div != 0:
-                divisible = False
-                break
-        if divisible:
+        if functools.reduce(lambda x, y: x and y, map(lambda x: num % x == 0, divisors)):
             result.append(num)
     return result
+
 lista1 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
 lista2 = [2, 3, 5]
 
 print(find_divisible_numbers(lista1, lista2))
+
--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -1,10 +1,13 @@
+import itertools
+
 def sum_pairs(numbers):
     pairs = set()
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            if numbers[i] + numbers[j] in numbers:
-                pairs.add(numbers[i] + numbers[j])
+    for i, j in itertools.combinations(numbers, 2):
+        if i + j in numbers:
+            pairs.add(i + j)
     return list(pairs)
+
 numbers = [1, 3, 5, 7, 9, 11, 12, 15, 16]
 result = sum_pairs(numbers)
-print(result)  
+print(result)
+
